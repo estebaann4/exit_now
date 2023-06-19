6b9e5a0b254c876a1945903b367058a7d121c2f4
estebaan444 <esteban.arquillo@institutvidreres.cat>
2023-06-19 11:53:03 +0200

    Adding firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 
 import 'src/helpers/dependency_injection.dart';
 import 'src/my_app.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   DependencyInjection.initialize();
   runApp(const MyApp());
